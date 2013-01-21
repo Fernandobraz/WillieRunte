00058e6cc2081e2cd387ce97a6f983364bbba13f
Fernando Braz <fernando.braz@innvent.com.br>
2013-01-21 14:32:18 -0200

    Cria essencial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WillieRunte::Application.config.secret_token = 'de715e377efc31f38734ccfe4bbcd8781417fe5c3193f05bce5adf45e81920458e998db30494c773a9416b7287205657aaf6f5717205322e6e53ea9d196eefcd'
